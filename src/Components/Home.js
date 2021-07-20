46e6fbe3401e9506536a446a70732e2f5fa999ba
Allard <AllardQuek@users.noreply.github.com>
2021-07-21 01:43:04 +0800

    update youtube channel url

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,100 +1,117 @@
-import styled from 'styled-components';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import GithubIcon from '@material-ui/icons/GitHub';
-import YoutubeIcon from '@material-ui/icons/YouTube';
-import Button from '@material-ui/core/Button';
-import Particle from './Particle';
-
+import styled from "styled-components";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GithubIcon from "@material-ui/icons/GitHub";
+import YoutubeIcon from "@material-ui/icons/YouTube";
+import Button from "@material-ui/core/Button";
+import Particle from "./Particle";
 
 const Home = () => {
-    return (
-        <HomeStyled>
-            <div className="p-particle-js">
-                <Particle />
-            </div>
-            <div className="typography">
-                <h1>Hi! I'm <span>Allard.</span></h1>
-                <p>
-                    Tech-lover | Musician | Educator
-                </p>
-                <div className="icons">
-                    <a href="https://www.linkedin.com/in/allard-quek/" target="_blank" rel="noopener noreferrer" className="icon i-linkedin">
-                        <LinkedInIcon />
-                    </a>
-                    <a href="https://github.com/AllardQuek" target="_blank" rel="noopener noreferrer" className="icon i-github">
-                        <GithubIcon />
-                    </a>
-                    <a href="https://www.youtube.com/channel/UCqGYtj5y-gIkTi4dSJfh7ow" target="_blank" rel="noopener noreferrer" className="icon i-youtube">
-                        <YoutubeIcon />
-                    </a>
-                </div>
+  return (
+    <HomeStyled>
+      <div className="p-particle-js">
+        <Particle />
+      </div>
+      <div className="typography">
+        <h1>
+          Hi! I'm <span>Allard.</span>
+        </h1>
+        <p>Tech-lover | Musician | Educator</p>
+        <div className="icons">
+          <a
+            href="https://www.linkedin.com/in/allard-quek/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon i-linkedin"
+          >
+            <LinkedInIcon />
+          </a>
+          <a
+            href="https://github.com/AllardQuek"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon i-github"
+          >
+            <GithubIcon />
+          </a>
+          <a
+            href="https://www.youtube.com/channel/ProgrammerError"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon i-youtube"
+          >
+            <YoutubeIcon />
+          </a>
+        </div>
 
-                <Button variant="contained" color="primary"> 
-                    <a href="https://docs.google.com/document/d/1dORKqrQx6a5h59FPkEVlp4vp263z1UrQVIGY4ESPf1k/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
-                        View Resume
-                    </a>
-                </Button>
-            </div>
-        </HomeStyled>
-    )
-}
+        <Button variant="contained" color="primary">
+          <a
+            href="https://docs.google.com/document/d/1dORKqrQx6a5h59FPkEVlp4vp263z1UrQVIGY4ESPf1k/edit?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View Resume
+          </a>
+        </Button>
+      </div>
+    </HomeStyled>
+  );
+};
 
+const HomeStyled = styled.header`
+  width: 100%;
+  height: 100vh;
+  position: relative;
 
-const HomeStyled =  styled.header`
-    width: 100%;
-    height: 100vh;
+  .p-particles-js {
     position: relative;
+    top: 0;
+    left: 0;
+  }
 
-    .p-particles-js {
-        position: relative;
-        top: 0;
-        left: 0;
+  .typography {
+    // Be mobile responsive
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    /* width: 100%; */
+  }
+
+  .icons {
+    margin-top: 0.5rem;
+    // * Not sure if needed for mobile responsiveness
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      margin: 0.5rem;
     }
 
-    .typography {
-        // Be mobile responsive
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        text-align: center;
-        /* width: 100%; */
+    .icon {
+      transition: all 0.4s ease-in-out;
+      cursor: pointer;
     }
 
-    .icons {
-        margin-top: 0.5rem;
-        // * Not sure if needed for mobile responsiveness
-        display: flex;
-        align-items: center;
-        justify-content: center;
-
-        svg {
-            margin: 0.5rem;
-        }
-
-        .icon {
-            transition: all 0.4s ease-in-out;
-            cursor: pointer;
-        }
-
-        .i-linkedin {
-            &:hover {
-                color: #0077b5;
-            }
-        }
-
-        .i-github {
-            &:hover {
-                color: #6e5494;
-            }
-        }
-
-        .i-youtube {
-            &:hover {
-                color: #FF0000;
-            }
-        }
+    .i-linkedin {
+      &:hover {
+        color: #0077b5;
+      }
     }
+
+    .i-github {
+      &:hover {
+        color: #6e5494;
+      }
+    }
+
+    .i-youtube {
+      &:hover {
+        color: #ff0000;
+      }
+    }
+  }
 `;
 
-export default Home
+export default Home;
